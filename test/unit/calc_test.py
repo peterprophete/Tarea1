86c0f8466020056d5e614be0646f8d34435b98ed
GiovannaLeon <giovannaleong@gmail.com>
2025-01-25 11:29:16 +0100

    Update calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -1,6 +1,8 @@
 import pytest
 import unittest
+
 from app.calc import Calculator
+
 
 @pytest.mark.unit
 class TestCalculate(unittest.TestCase):
@@ -17,16 +19,13 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(6, self.calc.add(3, 3))
         self.assertEqual(0, self.calc.add(2, -2))
         self.assertEqual(0, self.calc.add(-2, 2))
-        self.assertEqual(1, self.calc.add(1, 0))
-
+        self.assertEqual(1, self.calc.add(1, 0))      
+        
     def test_divide_method_returns_correct_result(self):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
-
-    def test_divide_by_zero(self):
-        self.assertRaises(ZeroDivisionError, self.calc.divide, 1, 0)  # Dividir por 0
-
+  
     def test_add_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.add, "2", 2)
         self.assertRaises(TypeError, self.calc.add, 2, "2")
@@ -35,6 +34,11 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.add, 2, None)
         self.assertRaises(TypeError, self.calc.add, object(), 2)
         self.assertRaises(TypeError, self.calc.add, 2, object())
+    
+    def test_divide_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.divide, "2", 2)
+        self.assertRaises(TypeError, self.calc.divide, 2, "2")
+        self.assertRaises(TypeError, self.calc.divide, "2", "2")
 
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
@@ -42,32 +46,21 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.multiply(-1, 0))
         self.assertEqual(-2, self.calc.multiply(-1, 2))
         self.assertRaises(TypeError, self.calc.multiply, "0", 0)
-
-    def test_multiply_method_fails_with_invalid_input(self):
-        self.assertRaises(TypeError, self.calc.multiply, [], 2)  # Lista
-        self.assertRaises(TypeError, self.calc.multiply, {}, 2)  # Diccionario
-        self.assertRaises(TypeError, self.calc.multiply, None, 2)  # None
-
+        
     def test_power_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.power(2, 2))
         self.assertEqual(1, self.calc.power(1, 0))
         self.assertEqual(1, self.calc.power(-1, 0))
         self.assertEqual(-27, self.calc.power(-3, 3))
         self.assertRaises(TypeError, self.calc.power, "0", 0)
-
-    def test_power_method_with_negative_exponent(self):
-        self.assertEqual(0.25, self.calc.power(2, -2))  # Exponente negativo
-        self.assertEqual(2, self.calc.power(2, -1))  # Exponente fraccionario
-
+        
     def test_substract_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.substract(10, 6))
         self.assertEqual(-2, self.calc.substract(256, 258))
         self.assertEqual(-1, self.calc.substract(-1, 0))
         self.assertEqual(0, self.calc.substract(0, 0))
+        self.assertEqual(0, self.calc.substract(0, 0))
         self.assertRaises(TypeError, self.calc.substract, "0", 0)
-
-    def test_substract_method_fails_with_invalid_input(self):
-        self.assertRaises(TypeError, self.calc.substract, {}, 1)  # Diccionario
-
-if __name__ == "__main__":
+        
+if __name__ == "__main__":  # pragma: no cover
     unittest.main()
