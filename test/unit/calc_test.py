84424764cadbf999e33f53315b384ba22e88f324
GiovannaLeon <giovannaleong@gmail.com>
2025-01-27 15:07:23 +0100

    Update calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -29,7 +29,7 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
-        self.assertRaises(ZeroDivisionError, self.calc.divide, 1, 0)  # División por cero
+
 
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
