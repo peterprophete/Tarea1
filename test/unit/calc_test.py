c5acdc60628639d2927d3cbf4855344c08866c37
GiovannaLeon <giovannaleong@gmail.com>
2025-01-29 14:20:30 +0100

    Update calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -1,12 +1,13 @@
 import pytest
 import unittest
+
 from app.calc import Calculator
+
 
 @pytest.mark.unit
 class TestCalculate(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
- 
 
     def test_add_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.add(2, 2))
@@ -18,20 +19,26 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(6, self.calc.add(3, 3))
         self.assertEqual(0, self.calc.add(2, -2))
         self.assertEqual(0, self.calc.add(-2, 2))
-        self.assertEqual(1, self.calc.add(1, 0))
+        self.assertEqual(1, self.calc.add(1, 0))      
         
     def test_divide_method_returns_correct_result(self):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
-
+        self.assertRaises(TypeError, self.calc.divide, "2", 2)
+  
+    def test_add_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.add, "2", 2)
+        self.assertRaises(TypeError, self.calc.add, 2, "2")
+        self.assertRaises(TypeError, self.calc.add, "2", "2")
+        self.assertRaises(TypeError, self.calc.add, None, 2)
+        self.assertRaises(TypeError, self.calc.add, 2, None)
+        self.assertRaises(TypeError, self.calc.add, object(), 2)
+        self.assertRaises(TypeError, self.calc.add, 2, object())
+    
     def test_divide_method_fails_with_nan_parameter(self):
-        # Test invalid input
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
-        # with self.assertRaises(ZeroDivisionError):
-        # self.calc.divide(1, 0)  # Expected to raise ZeroDivisionError
-
 
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
@@ -52,17 +59,8 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(-2, self.calc.substract(256, 258))
         self.assertEqual(-1, self.calc.substract(-1, 0))
         self.assertEqual(0, self.calc.substract(0, 0))
+        self.assertEqual(0, self.calc.substract(0, 0))
         self.assertRaises(TypeError, self.calc.substract, "0", 0)
         
-    def test_add_method_fails_with_nan_parameter(self):
-        # Test invalid input for add method
-        self.assertRaises(TypeError, self.calc.add, "2", 2)
-        self.assertRaises(TypeError, self.calc.add, 2, "2")
-        self.assertRaises(TypeError, self.calc.add, "2", "2")
-        self.assertRaises(TypeError, self.calc.add, None, 2)
-        self.assertRaises(TypeError, self.calc.add, 2, None)
-        self.assertRaises(TypeError, self.calc.add, object(), 2)
-        self.assertRaises(TypeError, self.calc.add, 2, object())
-
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
